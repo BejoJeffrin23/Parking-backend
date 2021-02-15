9c67bd60232eb6285446f2ec87d15fd6e9e82557
vivekvt <vivek78643@gmail.com>
2021-02-15 23:14:26 +0530

    faq topic query

--- lambda-functions/faq_lambda/index.js
+++ lambda-functions/faq_lambda/index.js
@@ -1,18 +1,21 @@
 const DB = require('../../utils/DB');
 const Faq = require('./utils/faqModel');
+const FaqTopic = require('./utils/faqTopicModel');
 DB();
 
 exports.handler = async (event) => {
   try {
     switch (event.type) {
+      case 'getAllFaqTopics':
+        return await FaqTopic.find();
       case 'getAllFaqs':
-        return await Faq.find();  
+        return await Faq.find();
       case 'getFaq':
         return await Faq.findById(event.arguments.id);
       case 'getFaqsByRole':
-        return await Faq.find({ roles:event.arguments.role }).exec();
+        return await Faq.find({ roles: event.arguments.role }).exec();
       case 'getFaqsByTopic':
-        return await Faq.find({ topic:event.arguments.topic }).exec();
+        return await Faq.find({ topic: event.arguments.topic }).exec();
       case 'createFaq':
         return await Faq.create(event.arguments);
       case 'updateFaq':
