e94de1c8476c976f5edcbaec79f920163edbd4ed
vivekvt <vivek78643@gmail.com>
2021-02-17 16:28:34 +0530

    bug fix

--- lambda-functions/user_lambda/signup.js
+++ lambda-functions/user_lambda/signup.js
@@ -4,80 +4,36 @@ const User = require('./utils/userModel');
 const cognito = require('../common_lambda/utils/Cognito');
 DB();
 
-exports.handler = async () => {
+exports.handler = async (event) => {
   try {
+    let existingUser = null;
     if (event.triggerSource) {
       console.log('Pre Sign up Event', event);
-      if (event.userName.includes('Facebook')) {
-        event.request.userAttributes.picture =
-          event.request.userAttributes.picture.data.url;
-      }
-      tempCognitoUser = await cognito.listUsers({
-        email: event.request.userAttributes.email,
-      });
-      userFlag =
-        tempCognitoUser &&
-        tempCognitoUser.Users &&
-        tempCognitoUser.Users[0] &&
-        tempCognitoUser.Users[0].Username;
-      if (!userFlag) {
-        if (event.triggerSource == 'PreSignUp_ExternalProvider') {
-          let [providerName, providerUserId] = event.userName.split('_');
-          let tempUsername = await cognito.adminCreateNativeUserAndLink({
-            name: event.request.userAttributes.name,
-            email: event.request.userAttributes.email,
-            picture: event.request.userAttributes.picture,
+      if (event.triggerSource == 'PreSignUp_ExternalProvider') {
+        existingUser = await cognito.listUsers({
+          email: event.request.userAttributes.email,
+        });
+        console.log('existingUser', existingUser);
+        console.log('event.userPoolId', event.userPoolId);
+        console.log('process.env.USER_POOL_ID', process.env.USER_POOL_ID);
+        console.log('check == ', event.userPoolId == process.env.USER_POOL_ID);
+        if (
+          existingUser &&
+          existingUser.Users &&
+          existingUser.Users.length > 0
+        ) {
+          const [providerName, providerUserId] = event.userName.split('_');
+          await cognito.linkProviderToUser({
+            username: existingUser.Users[0].Username,
             providerName,
             providerUserId,
           });
-          // tempUser = {
-          //   username: tempUsername,
-          //   name: event.request.userAttributes.name,
-          //   email: event.request.userAttributes.email,
-          //   picture: event.request.userAttributes.picture,
-          //   createdBy: event.triggerSource,
-          // };
-          // await User.create(tempUser);
           return event;
-          // return 'ACCOUNT_LINKED';
         } else {
-          tempUser = {
-            username: event.userName,
-            name: event.request.userAttributes.name,
-            email: event.request.userAttributes.email,
-            picture: event.request.userAttributes.picture,
-            createdBy: event.triggerSource,
-          };
-          await User.create(tempUser);
           return event;
         }
-      } else if (
-        userFlag &&
-        event.triggerSource == 'PreSignUp_ExternalProvider'
-      ) {
-        // Link User
-        let [providerName, providerUserId] = event.userName.split('_');
-        await cognito.linkProviderToUser({
-          username: tempCognitoUser.Users[0].Username,
-          providerName,
-          providerUserId,
-        });
-        // tempUser = {
-        //   username: event.userName,
-        //   name: event.request.userAttributes.name,
-        //   email: event.request.userAttributes.email,
-        //   picture: event.request.userAttributes.picture,
-        //   createdBy: event.triggerSource,
-        // };
-        // await User.create(tempUser);
-        // throw new Error('ACCOUNT_LINKED');
+      } else {
         return event;
-        // return 'ACCOUNT_LINKED';
-      } else if (
-        (userFlag && event.triggerSource == 'PreSignUp_SignUp') ||
-        event.triggerSource == 'PreSignUp_AdminCreateUser'
-      ) {
-        return 'User already exists with this email';
       }
     }
   } catch (error) {
